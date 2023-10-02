d24f409e47367668cfdef76ccb2bb072f4a0d477
MJRlabs <mijan@blackmesasystems.com>
2023-10-02 19:59:15 -0400

    deployment testing

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://mram:runnersRun5!@atlascluster.ipjheyc.mongodb.net/Travelers-Exchange?retryWrites=true&w=majority" || 'mongodb://127.0.0.1:27017/programming-thoughts');
+mongoose.connect("mongodb+srv://mram:runnersRun5!@atlascluster.ipjheyc.mongodb.net/?retryWrites=true&w=majority" || 'mongodb://127.0.0.1:27017/programming-thoughts');
 
 module.exports = mongoose.connection;
